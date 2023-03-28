04b4bbb00925cad5583c442f30694bd114a3130e
Randi Crews <randireneecrews@gmail.com>
2023-03-28 17:40:31 -0400

    name to initials

--- march 13 - 18.js
+++ march 13 - 18.js
@@ -51,3 +51,7 @@ function reverseList(list) {
   return backwards
 }
 
+//In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+
+
