e00ec920c8df9c2a444e7ff2b64d63f1405aed81
Randi Crews <randireneecrews@gmail.com>
2023-04-11 11:53:03 -0400

    8kyu

---
+++ 8kyu/count sheep.js
@@ -0,0 +1,20 @@
+// If you can't sleep, just count sheep!!
+
+// Task:
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+var countSheep = function (num){
+    //your code here
+    if (num != ''){
+    let sheeps = ''
+    let sheepNum = 0
+    do {
+      sheepNum = sheepNum + 1
+      sheeps = sheeps + `${sheepNum} sheep...`
+      
+    } while (sheepNum < num);
+      
+    return sheeps
+    }
+    else return ''
+    }
