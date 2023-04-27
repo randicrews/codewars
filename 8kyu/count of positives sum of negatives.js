616e20a0f15fd948260eb23aaf614514d9a042fb
Randi Crews <randireneecrews@gmail.com>
2023-04-27 15:15:55 -0400

    8

--- 8kyu/count of positives sum of negatives.js
+++ 8kyu/count of positives sum of negatives.js
@@ -8,9 +8,6 @@
 // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 
 function countPositivesSumNegatives(input) {
-    // your code here
-    // count of pos conj w
-    // sum of neg
     let negSum = 0
     if (input === null || input.length === 0) return []
     let negOnly = input.filter(num => num < 0)
