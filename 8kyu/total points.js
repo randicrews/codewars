8f1567975c5e983b0711dec51146cad64a22fe06
Randi Crews <randireneecrews@gmail.com>
2023-05-17 15:44:47 -0400

    7

--- 8kyu/total points.js
+++ 8kyu/total points.js
@@ -1,7 +1,5 @@
 // Our football team has finished the championship.
-
 // Our team's match results are recorded in a collection of strings. Each match is represented by a string in the format "x:y", where x is our team's score and y is our opponents score.
-
 // For example: ["3:1", "2:2", "0:1", ...]
 
 // Points are awarded for each match as follows:
