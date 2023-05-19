1870b15157d79808a5435cf45bee1ea1b1d68c52
Randi Crews <randireneecrews@gmail.com>
2023-05-19 10:51:45 -0400

    new wars

---
+++ 7kyu/alphabet war.js
@@ -0,0 +1,76 @@
+// DESCRIPTION:
+// Introduction
+// There is a war and nobody knows - the alphabet war!
+// There are two groups of hostile letters. The tension between left side letters and right side letters was too high and the war began.
+
+// Task
+// Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
+
+// The left side letters and their power:
+
+//  w - 4
+//  p - 3
+//  b - 2
+//  s - 1
+// The right side letters and their power:
+
+//  m - 4
+//  q - 3
+//  d - 2
+//  z - 1
+// The other letters don't have power and are only victims.
+
+// Example
+// alphabetWar("z");        //=> Right side wins!
+// alphabetWar("zdqmwpbs"); //=> Let's fight again!
+// alphabetWar("zzzzs");    //=> Right side wins!
+// alphabetWar("wwwwwwz");  //=> Left side wins!
+
+// left vs right
+// fight = string lowercase 
+// return winner
+// w p b s = left 
+// m q d z = right
+
+
+function alphabetWar(fight)
+{
+  fight = fight.split('')
+  let left = 0
+  let right = 0
+  for (const letter of fight){
+    if (letter == 'w'){
+      left += 4
+    }
+    if (letter == 'p'){
+      left += 3
+    }
+    if (letter == 'b'){
+      left += 2
+    }
+    if (letter == 's'){
+      left += 1
+    }
+    if (letter == 'm'){
+      right += 4
+    }
+    if (letter == 'q'){
+      right += 3
+    }
+    if (letter == 'd'){
+      right += 2
+    }
+    if (letter == 'z'){
+      right += 1
+    }
+  }
+  if ( left > right ){
+    return "Left side wins!"
+  } 
+  if ( left < right ){
+    return "Right side wins!"
+  } 
+  else {
+    return "Let's fight again!"
+  }
+}
---
+++ 8kyu/twice as old.js
@@ -0,0 +1,23 @@
+// DESCRIPTION:
+// Your function takes two arguments:
+
+// current father's age (years)
+// current age of his son (years)
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    // your code here
+    
+    // given dads age & sons age
+    // return the years from 'now' dad = son*2
+    // ex: 20, 0 --> 20
+    
+    let double = dadYearsOld - ( sonYearsOld * 2 )
+    
+    if ( double < 0 ){
+      double = double * -1
+    }
+    
+    return double
+    
+  }
