67c56df7d30747989c140423c81cd347ad7eeacc
Randi Crews <randireneecrews@gmail.com>
2023-07-11 13:04:14 -0400

    8

--- 8kyu/cookie eater.js
+++ 8kyu/cookie eater.js
@@ -6,3 +6,17 @@
 // Note: Make sure you return the correct message with correct spaces and punctuation.
 
 // Please leave feedback for this kata. Cheers!
+
+function cookie(x){
+    // if string zach
+    // if int or float moniCA
+    //else the dog
+    
+    if ( typeof x == 'string' ){
+      return 'Who ate the last cookie? It was Zach!'
+    } else if ( typeof x == 'number' ){
+      return 'Who ate the last cookie? It was Monica!'
+    } else {
+     return 'Who ate the last cookie? It was the dog!'
+    }
+  }
